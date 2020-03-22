3590f8d4ebbb28026c031b18f3b85e71585d93ac
gigigimay <mo.maytiya_st@tni.ac.th>
2020-03-23 01:14:48 +0700

    add temp title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.div`
+  font-size: 80px;
+  color: #eee;
+  font-weight: bold;
+  line-height: 1;
+
+  transition: .5s ease-in-out color;
+  :hover {
+    color: #71d9ce;
+  }
+`
 
 const App = () => (
-  <div className="App">
-    eiei
-  </div>
+  <Container>
+    <Title>gigigimay</Title>
+  </Container>
 )
 
 export default App
