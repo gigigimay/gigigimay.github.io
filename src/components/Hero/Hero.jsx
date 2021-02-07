385363d3bc0d90bd0cd77534b3e219c8ab96a684
gigigimay <mo.maytiya_st@tni.ac.th>
2021-02-07 18:57:32 +0700

    add hero content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,42 +1,36 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import Fade from 'react-reveal/Fade'
 import { Link } from 'react-scroll'
-import PortfolioContext from '../../context/context'
 
-const Header = () => {
-  const { hero } = useContext(PortfolioContext)
-  const { title, name, subtitle, cta } = hero
-
-  const [isDesktop, setIsDesktop] = useState(false)
+const useResponsive = () => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true)
-      setIsMobile(false)
-    } else {
-      setIsMobile(true)
-      setIsDesktop(false)
-    }
+    setIsMobile(window.innerWidth <= 769)
   }, [])
+
+  return { isMobile }
+}
+
+const Header = () => {
+  const { isMobile } = useResponsive()
 
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+        <Fade left={!isMobile} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            Hello, this is
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            <span className="text-color-main">.gigigimay</span>
           </h1>
         </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+        <Fade left={!isMobile} bottom={isMobile} duration={1000} delay={1000} distance="20px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                CLICK!
               </Link>
             </span>
           </p>
