1a044982f9fbd53dacbf5c039c1b822df3408b15
gigigimay <maytiya41@gmail.com>
2024-12-12 22:53:27 +0700

    add waves component

---
+++ src/components/Waves.tsx
@@ -0,0 +1,62 @@
+interface WavesProps {
+  fill?: string
+}
+
+export const Waves = ({ fill = '#ffffff' }: WavesProps) => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 scale-y-[60%] translate-y-[40%]">
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 32"
+        preserveAspectRatio="none"
+        shapeRendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g className="parallax">
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+            fill={fill}
+            fillOpacity={0.6}
+            // stroke="white"
+            // strokeWidth={0.03}
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="3"
+            fill={fill}
+            fillOpacity={0.4}
+            // stroke="white"
+            // strokeWidth={0.05}
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="5"
+            fill={fill}
+            fillOpacity={0.2}
+            // stroke="white"
+            // strokeWidth={0.07}
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="7"
+            fill={fill}
+            // stroke="white"
+            // strokeWidth={1.05}
+          />
+        </g>
+      </svg>
+    </div>
+  )
+}
