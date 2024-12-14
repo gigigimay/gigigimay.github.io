ba55cf527e51b1c4fce0bd5b4659165b2e16da50
gigigimay <maytiya41@gmail.com>
2024-12-15 02:18:38 +0700

    update menu labels

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -11,6 +11,6 @@ export const menuItems: MenuItemProps[] = [
   { icon: faHashtag, label: 'Home', link: '#top' },
   { icon: faUser, label: 'About Me', link: '#about-me' },
   { icon: faSuitcase, label: 'Experience & Education', link: '#experience' },
-  { icon: faCode, label: 'Side Projects', link: '#projects' },
-  { icon: faMailBulk, label: 'Contact', link: '#contact' },
+  { icon: faCode, label: 'Projects', link: '#projects' },
+  { icon: faMailBulk, label: 'Get In Touch', link: '#contact' },
 ]
